5049cf45054e1718f429c52e13d70313576ccf63
zed <zed@mail.ru>
2019-10-10 23:12:32 +0300

    added models

--- app/subapp/models.py
+++ app/subapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class PizzaShop(models.Model):
+    """Pizzzeria Class"""
+    name = models.CharField(max_length=50, verbose_name="Pizzeria")
+    description = models.TextField(verbose_name="Description")
+    rating = models.FloatField(verbose_name="Rating", default=0)
+    url = models.URLField(verbose_name="Link")
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    pizza_shop = models.ForeignKey(PizzaShop, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, verbose_name="Pizza Name")
+    description = models.TextField(verbose_name="Description")
+    price = models.FloatField(default=0, verbose_name="Pizza's Price")
+
+    def __str__(self):
+        return self.name
