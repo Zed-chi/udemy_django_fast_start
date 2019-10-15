5a469d01bec3a5d7f7ef10863fd61a09c51e1840
alex <zed-chi@mail.ru>
2019-10-15 13:00:52 +0300

    added verific-n

--- pizzashopproject/pizzashopapp/views.py
+++ pizzashopproject/pizzashopapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from pizzashopapp.forms import UserForm, PizzaShopForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def home(req):
@@ -16,6 +18,19 @@ def back_home(req):
 def sign_up(req):
     user_form = UserForm()
     pizza_shop_form = PizzaShopForm()
+    if req.method == "POST":
+        user_form = UserForm(req.POST)
+        pizza_shop_form = PizzaShopForm(req.POST, req.FILES)
+        if user_form.is_valid() and pizza_shop_form.is_valid():
+            new_user = User.objects.create_user(**user_form.cleaned_data)
+            new_pizzashop = pizza_shop_form.save(commit=False)
+            new_pizzashop.owner = new_user
+            new_pizzashop.save()
+            login(req, authenticate(
+                username=user_form.cleaned_data["username"],
+                password=user_form.cleaned_data["password"],
+            ))
+            return redirect(back_home)
 
     return render(
         req, 
