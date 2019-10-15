d1d326604369709f8e28d0988bf79f2e517b10ca
alex <zed-chi@mail.ru>
2019-10-15 13:00:29 +0300

    forms upd

--- pizzashopproject/pizzashopapp/forms.py
+++ pizzashopproject/pizzashopapp/forms.py
@@ -4,9 +4,12 @@ from pizzashopapp.models import PizzaShop
 
 
 class UserForm(forms.ModelForm):
+    username = forms.CharField(max_length=255, required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ("username", "password", "first_name", "last_name", "email")
+
 
 
 class PizzaShopForm(forms.ModelForm):
